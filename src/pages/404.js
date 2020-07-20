58b0d0a75d4bb70902ccda5f3bded50cff1b4f22
hirokicodes <hirokicodes@gmail.com>
2020-07-21 01:36:29 +0900

    Added 404 page.

---
+++ src/pages/404.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => {
+  return (
+    <Layout>
+      <div className="flex flex-col h-full justify-center items-center">
+        <div className="text-text-primary text-5xl font-semibold">
+          Page not found
+        </div>
+        <div className="text-text-primary text-3xl">
+          Oops! The page you are looking for does not exist.
+        </div>
+      </div>
+    </Layout>
+  )
+}
