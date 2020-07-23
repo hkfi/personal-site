18e9746625b58597fc0026dbfea0d8b31b4f6b3e
Hiroki Furuichi <hirokicodes@gmail.com>
2020-07-23 22:07:43 +0900

    Implemented scroll reveal for Blog page. Ref doesn't work on AniLink for some reason.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,12 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Layout from "../components/layout"
 import { PageTitle } from "../components/pageTitle"
+import { sr, srConfig } from "../utils/sr"
 
 export default () => {
-  const data = useStaticQuery(graphql`
+  const { allContentfulBlogPost } = useStaticQuery(graphql`
     query {
       allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
@@ -19,21 +20,33 @@ export default () => {
     }
   `)
 
+  const revealBlogPosts = useRef([])
+  useEffect(() => {
+    revealBlogPosts.current.forEach((ref, i) =>
+      sr.reveal(ref, srConfig(i * 100))
+    )
+  }, [])
+
   return (
     <Layout>
       <PageTitle title={"Blog"} />
 
-      {data.allContentfulBlogPost.edges.map((edge, i) => {
+      {allContentfulBlogPost.edges.map((edge, i) => {
         return (
-          <AniLink
-            fade
-            className="w-full max-w-4xl flex m-2 p-2 text-text-primary hover:text-theme-primary justify-between bg-background-secondary"
-            to={`/blog/${edge.node.slug}`}
+          <div
             key={i}
+            ref={el => (revealBlogPosts.current[i] = el)}
+            className="w-full max-w-4xl flex m-2 bg-background-secondary"
           >
-            <div className="text-3xl">{edge.node.title}</div>
-            <div className="text-2xl">{edge.node.publishedDate}</div>
-          </AniLink>
+            <AniLink
+              fade
+              to={`/blog/${edge.node.slug}`}
+              className="w-full h-full flex p-4 justify-between bg-background-secondary"
+            >
+              <div className="text-3xl">{edge.node.title}</div>
+              <div className="text-2xl">{edge.node.publishedDate}</div>
+            </AniLink>
+          </div>
         )
       })}
     </Layout>
